d899ddc46d43611862314636ade220f6c54d17d0
lYesterdaYl <bgmds@126.com>
2018-10-03 12:07:27 -0700

    feat
    update new world_map class for generating world map with previous gathered information

--- src/MyAI.py
+++ src/MyAI.py
@@ -25,6 +25,7 @@ class MyAI ( Agent ):
         self._current_direction = ''
         self._number_of_move = 0
         self._current_status = {}
+        self._world_map = World_Map()
 
     def store_information(self, stench, breeze, glitter, bump, scream):
         self._current_status['stench'] = stench
@@ -32,26 +33,44 @@ class MyAI ( Agent ):
         self._current_status['glitter'] = glitter
         self._current_status['bump'] = bump
         self._current_status['scream'] = scream
-        print(self.__current_status)
+        print(self._current_status)
 
 
     def analysis(self):
-        if(self.__number_of_move == 0 and self.__current_status['breeze'] == True):
+        if(self._number_of_move == 0 and self._current_status['breeze'] == True):
             return Agent.Action.CLIMB
 
-
+    def found_gold(self):
+        if(self._current_status['glitter']):
+            return True
+        else:
+            return False
 
     def getAction( self, stench, breeze, glitter, bump, scream ):
         self.store_information(stench, breeze, glitter, bump, scream)
+        self._world_map.update_current_position(self._current_status)
+        self._world_map.show_map()
 
         feedback = self.analysis()
 
 
-        self.__number_of_move += 1
+        self._number_of_move += 1
         return feedback
 
-class Map:
+class World_Map:
 
     def __init__(self):
         self._x = 0
         self._y = 0
+        self._map = [["" for x in range(4)] for x in range(4)]
+
+    def show_map(self):
+        # self._map[0][1] = 2
+        print("world map is ", self._map)
+        # print(self._map[0][0])
+
+    def update_current_position(self, current_status):
+        if current_status['stench']: self._map[self._x][self._y] += ' S '
+        if current_status['breeze']: self._map[self._x][self._y] += ' B '
+        if current_status['glitter']: self._map[self._x][self._y] += ' G '
+
