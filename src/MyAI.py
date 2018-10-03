33bf6064f89bb3aa620c0b43750100b1376e1fb7
lYesterdaYl <bgmds@126.com>
2018-10-03 13:06:42 -0700

    feat
    updated some new variable and function for world class.

--- src/MyAI.py
+++ src/MyAI.py
@@ -22,8 +22,6 @@ from Agent import Agent
 class MyAI ( Agent ):
 
     def __init__ ( self ):
-        self._current_direction = ''
-        self._number_of_move = 0
         self._current_status = {}
         self._world_map = World_Map()
 
@@ -33,12 +31,8 @@ class MyAI ( Agent ):
         self._current_status['glitter'] = glitter
         self._current_status['bump'] = bump
         self._current_status['scream'] = scream
+
         print(self._current_status)
-
-
-    def analysis(self):
-        if(self._number_of_move == 0 and self._current_status['breeze'] == True):
-            return Agent.Action.CLIMB
 
     def found_gold(self):
         if(self._current_status['glitter']):
@@ -49,12 +43,11 @@ class MyAI ( Agent ):
     def getAction( self, stench, breeze, glitter, bump, scream ):
         self.store_information(stench, breeze, glitter, bump, scream)
         self._world_map.update_current_position(self._current_status)
+        self._world_map.swap_position()
         self._world_map.show_map()
 
-        feedback = self.analysis()
+        feedback = self._world_map.analysis()
 
-
-        self._number_of_move += 1
         return feedback
 
 class World_Map:
@@ -62,15 +55,32 @@ class World_Map:
     def __init__(self):
         self._x = 0
         self._y = 0
+        self._current_direction = ''
+        self._number_of_move = 0
+        self._current_status = {}
         self._map = [["" for x in range(4)] for x in range(4)]
 
     def show_map(self):
-        # self._map[0][1] = 2
+        self._map[0][1] = 2
         print("world map is ", self._map)
         # print(self._map[0][0])
 
+    def swap_position(self):
+        temp = self._map[0]
+        self._map[0] = self._map[3]
+        self._map[3] = temp
+
+        temp = self._map[1]
+        self._map[1] = self._map[2]
+        self._map[2] = temp
+
     def update_current_position(self, current_status):
-        if current_status['stench']: self._map[self._x][self._y] += ' S '
-        if current_status['breeze']: self._map[self._x][self._y] += ' B '
-        if current_status['glitter']: self._map[self._x][self._y] += ' G '
+        if current_status['stench']: self._map[self._y][self._x] += ' S '
+        if current_status['breeze']: self._map[self._y][self._x] += ' B '
+        if current_status['glitter']: self._map[self._y][self._x] += ' G '
+        self._current_status = current_status
+
+    def analysis(self):
+        if (self._number_of_move == 0 and self._current_status['breeze'] == True):
+            return Agent.Action.CLIMB
 
