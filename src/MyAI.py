39ad3481b91c75000d6ed716f71cdb65d2d197e1
李艺安 <yianl2@uci.edu>
2018-10-03 16:44:50 -0700

    update

--- src/MyAI.py
+++ src/MyAI.py
@@ -24,7 +24,7 @@ class MyAI ( Agent ):
     def __init__ ( self ):
         self._current_status = {}
         self._world_map = World_Map()
-
+        self.tmp=[]
 
 
     def store_information(self, stench, breeze, glitter, bump, scream):
@@ -48,6 +48,10 @@ class MyAI ( Agent ):
         self._world_map.update_current_position(self._current_status)
         self._world_map.swap_position()
         self._world_map.show_map()
+        if self.tmp == []:
+            self.tmp = self._world_map.moveTo(0,1)
+            self.tmp = self._world_map.moveTo(1,1)
+        return self._world_map.actionlist.pop(0)
 
 
 class World_Map:
@@ -56,66 +60,70 @@ class World_Map:
         self._y = 0
         self._map = [["" for x in range(4)] for x in range(4)]
 
+        self._number_of_move = 0
+        self._current_direction = 'right'
         self._safe_position = [[False for x in range(4)] for y in range(4)]
         self._breeze_position = [[False for x in range(4)] for y in range(4)]
         self._stench_position = [[False for x in range(4)] for y in range(4)]
         self._Pit_position = [[False for x in range(4)] for y in range(4)]
         self.current_position = (0, 0)
+        self.actionlist=[]
 
     def moveTo(self, dx, dy):
         x, y = self.current_position
         # go left
         if dx < x:
             if self._current_direction == 'right':
-                Agent.Action.TURN_RIGHT
-                Agent.Action.TURN_RIGHT
+                self.actionlist.append(Agent.Action.TURN_RIGHT)
+                self.actionlist.append(Agent.Action.TURN_RIGHT)
             if self._current_direction == 'up':
-                Agent.Action.TURN_LEFT
+                self.actionlist.append(Agent.Action.TURN_LEFT)
             if self._current_direction == 'down':
-                Agent.Action.TURN_RIGHT
+                self.actionlist.append(Agent.Action.TURN_RIGHT)
             self._current_direction = 'left'
-            Agent.Action.FORWARD
+            self.actionlist.append(Agent.Action.FORWARD)
             self.current_position = (x - 1, y)
             self._number_of_move += 1
         # go right
         if dx > x:
             if self._current_direction == 'left':
-                Agent.Action.TURN_LEFT
-                Agent.Action.TURN_LEFT
+                self.actionlist.append(Agent.Action.TURN_LEFT)
+                self.actionlist.append(Agent.Action.TURN_LEFT)
             if self._current_direction == 'up':
-                Agent.Action.TURN_RIGHT
+                self.actionlist.append(Agent.Action.TURN_RIGHT)
             if self._current_direction == 'down':
-                Agent.Action.TURN_LEFT
+                self.actionlist.append(Agent.Action.TURN_LEFT)
             self._current_direction = 'right'
-            Agent.Action.FORWARD
+            self.actionlist.append(Agent.Action.FORWARD)
             self.current_position = (x + 1, y)
             self._number_of_move += 1
         # go up
         if dy > y:
             if self._current_direction == 'left':
-                Agent.Action.TURN_RIGHT
+                self.actionlist.append(Agent.Action.TURN_RIGHT)
             if self._current_direction == 'right':
-                Agent.Action.TURN_LEFT
+                self.actionlist.append(Agent.Action.TURN_LEFT)
             if self._current_direction == 'down':
-                Agent.Action.TURN_LEFT
-                Agent.Action.TURN_LEFT
+                self.actionlist.append(Agent.Action.TURN_LEFT)
+                self.actionlist.append(Agent.Action.TURN_LEFT)
             self._current_direction = 'up'
-            Agent.Action.FORWARD
+            self.actionlist.append(Agent.Action.FORWARD)
             self.current_position = (x, y + 1)
             self._number_of_move += 1
         # go down
         if dy < y:
             if self._current_direction == 'left':
-                Agent.Action.TURN_RIGHT
+                self.actionlist.append(Agent.Action.TURN_RIGHT)
             if self._current_direction == 'right':
-                Agent.Action.TURN_LEFT
+                self.actionlist.append(Agent.Action.TURN_LEFT)
             if self._current_direction == 'down':
-                Agent.Action.TURN_LEFT
-                Agent.Action.TURN_LEFT
+                self.actionlist.append(Agent.Action.TURN_LEFT)
+                self.actionlist.append(Agent.Action.TURN_LEFT)
             self._current_direction = 'down'
-            Agent.Action.FORWARD
+            self.actionlist.append(Agent.Action.FORWARD)
             self.current_position = (x, y - 1)
             self._number_of_move += 1
+        print('move:' , self.actionlist)
     def show_map(self):
         # self._map[0][1] = 2
         print("world map is ", self._map)
